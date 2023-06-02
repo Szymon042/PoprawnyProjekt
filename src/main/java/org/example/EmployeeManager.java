8092ea9eb99b0b49ef80e4fd0cfab2836f75e343
Szymon042 <szymon.ordon2@gmail.com>
2023-06-02 11:50:16 +0200

    Dodano możliwość dodawania pracowników

--- src/main/java/org/example/EmployeeManager.java
+++ src/main/java/org/example/EmployeeManager.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class EmployeeManager {
+    private final HashMap<Integer, Employee> employees;
+
+    public HashMap<Integer, Employee> getEmployees() {
+        return this.employees;
+    }
+
+    public EmployeeManager(HashMap<Integer, Employee> employees) {
+        this.employees = employees;
+    }
+
+    public void addEmployee(String firstName, String lastName, String position, int salary) {
+        Employee employee = new Employee(firstName, lastName, position, salary);
+        Integer keyEmployee = employee.getEmployeeID();
+        this.employees.put(keyEmployee, employee);
+    }
+
+
+
+
+
 }
